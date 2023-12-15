94a52090e1e92e013675de1622cbc792de483cf1
zastrow <philip@zastrow.co>
2023-12-14 20:37:02 -0500

    fix: collection globs

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,35 @@
 require('dotenv').config();
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const fg = require('fast-glob').sync;
 
 module.exports = (eleventyConfig) => {
 	eleventyConfig.addCollection('posts', (api) => {
-		return api.getFilteredByGlob("src/content/posts/*.md").sort((a,b) => b.date - a.date);
+		const files = fg(["src/content/posts/**/*.md"], {
+			ignore: ["src/content/posts/index.md"]
+		});
+		return api.getFilteredByGlob(files).sort((a,b) => b.date - a.date);
 	});
 
 	eleventyConfig.addCollection('books', (api) => {
-		return api.getFilteredByGlob("src/content/books/*.md").sort((a,b) => b.date - a.date);
+		const files = fg(["src/content/books/**/*.md"], {
+			ignore: ["src/content/books/index.md"]
+		});
+		return api.getFilteredByGlob(files).sort((a,b) => b.date - a.date);
 	});
 
 	eleventyConfig.addCollection('micro', (api) => {
-		return api.getFilteredByGlob("src/content/micro/*.md").sort((a,b) => b.date - a.date);
+		const files = fg(["src/content/micro/**/*.md"], {
+			ignore: ["src/content/micro/index.md"]
+		});
+		return api.getFilteredByGlob(files).sort((a,b) => b.date - a.date);
 	});
 
 	eleventyConfig.addCollection('all', (api) => {
-		return api.getFilteredByGlob(["src/content/posts/*.md", "src/content/books/*.md"]).sort((a,b) => b.date - a.date);
+
+		const files = fg(["src/content/posts/**/*.md", "src/content/books/**/*.md"], {
+			ignore: ["src/content/posts/index.md", "src/content/books/index.md"]
+		});
+		return api.getFilteredByGlob(files).sort((a,b) => b.date - a.date);
 	});
 
 	eleventyConfig.addGlobalData('site_url', process.env.SITE_URL);
