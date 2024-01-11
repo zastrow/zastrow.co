14a6d1b36349c06cdc99eb2e9df88a46364bb30c
zastrow <philip@zastrow.co>
2024-01-11 07:42:07 -0500

    feat: random external article

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,6 +1,8 @@
 import './html-init';
 import resizeWatcher from './resize';
 import navSetup from './nav';
+import articleUpdater from './article-getter';
 
 navSetup();
 resizeWatcher();
+articleUpdater();
