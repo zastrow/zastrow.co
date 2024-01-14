e08cb95b9e3153b74c0283291f8704edb0a54ff4
zastrow <philip@zastrow.co>
2024-01-13 20:19:01 -0500

    feat: Add color mode cookie

--- src/scripts/color-mode.js
+++ src/scripts/color-mode.js
@@ -6,25 +6,56 @@ const buttonAutoMode = document.querySelector('.js-mode-auto');
 const allButtons = [buttonDarkMode, buttonAutoMode, buttonLightMode];
 const deactivateButtons = () => allButtons.forEach(e => e.classList.remove('cmp-settings__mode-button--active'));
 
-buttonAutoMode.classList.add('cmp-settings__mode-button--active');
-
-buttonDarkMode.addEventListener('click', () => {
+const setDarkMode = () => {
 	deactivateButtons();
 	body.classList.remove('mode-light');
 	body.classList.add('mode-dark');
 	buttonDarkMode.classList.add('cmp-settings__mode-button--active');
-});
+}
 
-buttonLightMode.addEventListener('click', () => {
+const setLightMode = () => {
 	deactivateButtons();
 	body.classList.remove('mode-dark');
 	body.classList.add('mode-light');
 	buttonLightMode.classList.add('cmp-settings__mode-button--active');
-});
+}
 
-buttonAutoMode.addEventListener('click', () => {
+const setAutoMode = () => {
 	deactivateButtons();
 	body.classList.remove('mode-dark');
 	body.classList.remove('mode-light');
 	buttonAutoMode.classList.add('cmp-settings__mode-button--active');
+}
+
+const expandedCookieValue = document.cookie
+	.split('; ')
+	.find((row) => row.startsWith('zastrowcoColorMode='))
+	?.split('=')[1];
+
+if (expandedCookieValue === 'dark') {
+	setDarkMode();
+}
+else if (expandedCookieValue === 'light') {
+	setLightMode();
+}
+else if (expandedCookieValue === 'auto') {
+	setAutoMode();
+}
+else {
+	buttonAutoMode.classList.add('cmp-settings__mode-button--active');
+}
+
+buttonDarkMode.addEventListener('click', () => {
+	setDarkMode();
+	document.cookie = `zastrowcoColorMode=dark;`;
+});
+
+buttonLightMode.addEventListener('click', () => {
+	setLightMode();
+	document.cookie = `zastrowcoColorMode=light;`;
+});
+
+buttonAutoMode.addEventListener('click', () => {
+	setAutoMode();
+	document.cookie = `zastrowcoColorMode=auto;`;
 });
